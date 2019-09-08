c729622a9d7dd2ea5c09b62fd283e38f0376a2c2
Per Olav Flaten <perolavflaten@live.no>
2019-09-08 15:06:21 +0200

    test

--- scripts/build-monogame-pipeline.sh
+++ scripts/build-monogame-pipeline.sh
@@ -8,6 +8,6 @@ fi
 if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
   dotnet build $(dirname $0)/../src/Hero6.MonoGamePipeline.sln -c $CONFIG
 else
-  xbuild $(dirname $0)/../src/Hero6.MonoGamePipeline.sln /t:restore /property:Configuration=$CONFIG
+  # xbuild $(dirname $0)/../src/Hero6.MonoGamePipeline.sln /t:restore /property:Configuration=$CONFIG
   xbuild $(dirname $0)/../src/Hero6.MonoGamePipeline.sln /property:Configuration=$CONFIG
 fi
