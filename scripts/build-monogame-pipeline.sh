da6787ba74b987a2e9d160007e20df34626b72aa
Per Olav Flaten <perolavflaten@live.no>
2019-09-08 17:55:36 +0200

    Test package restore

--- scripts/build-monogame-pipeline.sh
+++ scripts/build-monogame-pipeline.sh
@@ -5,9 +5,11 @@ if [ -x $CONFIG ]; then
   exit 1
 fi
 
+dotnet restore $(dirname $0)/../src/Hero6.sln -c $CONFIG
+
 if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
   dotnet build $(dirname $0)/../src/Hero6.MonoGamePipeline/Hero6.MonoGamePipeline.csproj -c $CONFIG
 else
-  msbuild $(dirname $0)/../src/Hero6.MonoGamePipeline.sln /t:restore /property:Configuration=$CONFIG
+  # msbuild $(dirname $0)/../src/Hero6.MonoGamePipeline.sln /t:restore /property:Configuration=$CONFIG
   msbuild $(dirname $0)/../src/Hero6.MonoGamePipeline/Hero6.MonoGamePipeline.csproj /property:Configuration=$CONFIG
 fi
